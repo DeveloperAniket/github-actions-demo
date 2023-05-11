da891f6d909f65d418266c544647fe8e2bef0be0
Aniket Samanta <aniket.samanta@coxautoinc.com>
2023-05-11 17:13:35 +0530

    test

--- .github/workflows/actions/hello/index.js
+++ .github/workflows/actions/hello/index.js
@@ -1,1 +1,16 @@
-asa
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    //throw new Error("Custom Error from my action");
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    
+    const payload = JSON.stringify(github, null, "\t");
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
