3cab16d643c072b2a51117ce77b777565807e074
Aniket Samanta <aniket.samanta@coxautoinc.com>
2023-05-11 17:43:44 +0530

    Time

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
     //throw new Error("Custom Error from my action");
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
 
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    
+    console.log(`time in action log ${time}!`);
+
+
     const payload = JSON.stringify(github, null, "\t");
     console.log(`The event payload: ${payload}`);
+
 } catch (error) {
     core.setFailed(error.message);
 }
